e499596416d101ae39406e5b9a74e2b5a3e197f8
Guilherme Munarolo <guimunarolo@hotmail.com>
2020-07-23 08:41:10 -0300

    improve BaseHandler

--- python/solution/handlers.py
+++ python/solution/handlers.py
@@ -1,20 +1,27 @@
+import uuid
+
+
 class BaseHandler:
     schema_class = None
 
-    def _build_parsed_message(self, message):
-        return self.schema_class(**message)
+    def _build_action_kwargs(self, metadata, message):
+        kwargs = {"metadata": metadata}
+        schema_name = metadata.event_schema
 
-    def _get_message_argument_name(self):
-        schema_name = self.schema_class.__name__.lower()
-        return schema_name
+        if metadata.event_action == "deleted":
+            kwargs[f"{schema_name}_id"] = uuid.UUID(message[0])
+        elif metadata.event_action == "removed":
+            kwargs["parent_id"] = uuid.UUID(message[0])
+            kwargs[f"{schema_name}_id"] = uuid.UUID(message[1])
+        else:
+            kwargs[schema_name] = self.schema_class.build_from_message(message)
 
-    def handle(self, event, message):
-        parsed_message = self._build_parsed_message(message)
-        message_arg_name = self._get_message_argument_name()
+        return kwargs
 
+    def handle(self, metadata, message):
         try:
-            action_method = getattr(self, f"process_{event['event_action']}")
+            action_method = getattr(self, f"process_{metadata.event_action}")
         except AttributeError:
             raise NotImplementedError()
 
-        return action_method(**{"event": event, message_arg_name: parsed_message})
+        return action_method(**self._build_action_kwargs(metadata, message))
--- python/tests/test_handlers.py
+++ python/tests/test_handlers.py
@@ -1,26 +1,62 @@
+import uuid
 from unittest import mock
 
 import pytest
 
 from solution.handlers import BaseHandler
-
-
-@pytest.fixture
-def base_handler():
-    handler = BaseHandler()
-    handler._build_parsed_message = mock.Mock(return_value={})
-    handler._get_message_argument_name = mock.Mock(return_value="test")
-    handler.process_test = mock.Mock(return_value=True)
-    return handler
+from solution.schemas import EventMetadata, Proposal
 
 
 class TestBaseHandler:
-    def test_handle_calls_processor_with_params(self, base_handler, event_data):
-        assert base_handler.handle(event=event_data, message={}) is True
-        base_handler.process_test.assert_called_once_with(event=event_data, test={})
+    def test_build_action_kwargs_with_deleted_action(self, event_data):
+        event_data["event_schema"] = "foo"
+        event_data["event_action"] = "deleted"
+        metadata = EventMetadata(**event_data)
+        some_id = uuid.uuid4()
+        message = [str(some_id)]
 
-    def test_handle_raises_not_implemented_whith_uncovered_event_action(self, base_handler, event_data):
+        expected_result = {"metadata": metadata, "foo_id": some_id}
+        assert BaseHandler()._build_action_kwargs(metadata, message) == expected_result
+
+    def test_build_action_kwargs_with_removed_action(self, event_data):
+        event_data["event_schema"] = "foo"
+        event_data["event_action"] = "removed"
+        metadata = EventMetadata(**event_data)
+        some_id = uuid.uuid4()
+        some_other_id = uuid.uuid4()
+        message = [str(some_id), str(some_other_id)]
+
+        expected_result = {"metadata": metadata, "parent_id": some_id, "foo_id": some_other_id}
+        assert BaseHandler()._build_action_kwargs(metadata, message) == expected_result
+
+    @pytest.mark.parametrize("common_action", ("created", "added", "updated"))
+    def test_build_action_kwargs_with_common_action(self, common_action, event_data, proposal_data):
+        event_data["event_schema"] = "foo"
+        event_data["event_action"] = common_action
+        metadata = EventMetadata(**event_data)
+        message = list(proposal_data.values())
+        BaseHandler.schema_class = Proposal
+
+        expected_result = {"metadata": metadata, "foo": Proposal(**proposal_data)}
+        assert BaseHandler()._build_action_kwargs(metadata, message) == expected_result
+
+    def test_handle_calls_processor(self, event_data):
+        event_data["event_action"] = "created"
+        metadata = EventMetadata(**event_data)
+        base_handler = BaseHandler()
+        base_handler.process_created = mock.Mock(return_value=None)
+        base_handler._build_action_kwargs = mock.Mock(return_value={"metadata": metadata, "foo": None})
+
+        assert base_handler.handle(metadata, message={}) is None
+        assert base_handler.process_created.called is True
+        assert base_handler._build_action_kwargs.called is True
+
+    def test_handle_raises_not_implemented_whith_uncovered_event_action(self, event_data):
         event_data["event_action"] = "uncovered"
+        metadata = EventMetadata(**event_data)
+        base_handler = BaseHandler()
+        base_handler._build_action_kwargs = mock.Mock(return_value={"metadata": metadata, "foo": None})
 
         with pytest.raises(NotImplementedError):
-            base_handler.handle(event=event_data, message={})
+            base_handler.handle(metadata, message={})
+            assert base_handler._build_action_kwargs.called is False
